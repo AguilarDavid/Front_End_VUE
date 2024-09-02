d550e4602e08c738b3950566d30ca63dbe5f5549
David Aguilar <131323318+AguilarDavid@users.noreply.github.com>
2024-09-02 19:32:29 -0400

    Delete M2_Drilling_Final directory

--- M2_Drilling_Final/script.js
+++
@@ -1,9 +0,0 @@
-function validarCorreo(){
-    var correo = document.getElementById(correo).ariaValueMax;
-    var regexCorreo = ^[^@]+@[^@]+\.[a-zA-Z]{2,}$;
-    if(regexCorreo.toLocaleString(correo)) {
-        alert('Correo no válido');
-    }else{
-        alert('Correo no válido');
-    }
-}
